b61de09f17dc45d3d6ad980f48c26ed3655377bf
Georgem Arruda <georgem.juniore@gmail.com>
2024-06-21 10:21:47 -0300

    Atualiza pesquisa nova estrutura (#11)
    * Atualização dos testes de pesquisa para nova estrutura
    
    * Inclusao de testes de deleção

---
+++ config/structs/structPesquisa.go
@@ -0,0 +1,51 @@
+package structs
+
+type TipoQuestao string
+
+const (
+	SUBJETIVA TipoQuestao = "SUBJETIVA"
+)
+
+type Colaboradores struct {
+	Matricula        string `json:"Matricula"`
+	CPF              string `json:"CPF"`
+	NrInscEmpregador string `json:"NrInscEmpregador"`
+}
+
+type Pergunta struct {
+	ID          string                   `json:"id"`
+	Tipo        TipoQuestao              `json:"tipo"`
+	Ordem       string                   `json:"ordem"`
+	Texto       string                   `json:"texto"`
+	Obrigatoria bool                     `json:"obrigatoria"`
+	NotaMinima  int                      `json:"notaMinima"`
+	NotaMaxima  int                      `json:"notaMaxima"`
+	Respostas   []map[string]interface{} `json:"respostas"`
+}
+
+type Pesquisa struct {
+	ID                 string          `json:"id"`
+	Inicio             string          `json:"inicio"`
+	Fim                string          `json:"fim"`
+	NrInscEmpregador   string          `json:"NrInscEmpregador"`
+	Titulo             string          `json:"titulo"`
+	MonitoramentoSaude bool            `json:"monitoramentoSaude"`
+	PesquisaAnonima    bool            `json:"pesquisaAnonima"`
+	IndependeMatricula bool            `json:"independeMatricula"`
+	Versao             string          `json:"Versao"`
+	Perguntas          []Pergunta      `json:"perguntas"`
+	Colaboradores      []Colaboradores `json:"colaboradores"`
+}
+
+//STRUCT DE DELEÇÃO DE PESQUISA
+
+type PesquisaDeletada struct {
+	Matricula        string `json:"Matricula"`
+	CPF              string `json:"CPF"`
+	NrInscEmpregador string `json:"NrInscEmpregador"`
+	PesquisaId       string `json:"PesquisaId"`
+}
+
+type DeletePesquisa struct {
+	Pesquisas []PesquisaDeletada `json:"pesquisas"`
+}
