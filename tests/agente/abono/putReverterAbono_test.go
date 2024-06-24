7a7d33bfa78e6dbfc9a93d56cbdc302dda3a0c79
Ton Arruda <georgem.juniore@gmail.com>
2024-06-24 14:43:07 -0300

    Ajuste Put Reverter Abono

--- tests/agente/abono/putReverterAbono_test.go
+++ tests/agente/abono/putReverterAbono_test.go
@@ -5,48 +5,45 @@ import (
 	"testing"
 
 	"github.com/patriciapersi/colabore-api/config"
+	agentebody "github.com/patriciapersi/colabore-api/config/agenteBody"
+	"github.com/patriciapersi/colabore-api/config/structs"
+	helper "github.com/patriciapersi/colabore-api/helper"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPutReveterAbono(t *testing.T) {
+const (
+	cnpj  = "10821992"
+	taxID = "60515860409"
+	matID = "000031"
+)
+
+func TestPutReveterAbonoo(t *testing.T) {
 
 	testCases := []struct {
 		description  string
-		header       map[string]string
-		setupBody    bool
+		setupHeaders map[string]string
+		requestBody  structs.PutAbonoBody
 		expected     int
 		expectedDesc string
-		nrInsc       string
-		cpf          string
-		matricula    string
-		precondition bool
 	}{
 		{
 			description:  "Reverter o status de um Abono para pendente",
-			header:       config.SetupHeadersAgente(),
-			setupBody:    true,
+			setupHeaders: config.SetupHeadersAgente(),
+			requestBody:  agentebody.PutReverterSolicitacaoAbono(cnpj, taxID, matID),
 			expected:     http.StatusOK,
 			expectedDesc: "Sucesso",
-			nrInsc:       "10821992",
-			cpf:          "60515860409",
-			matricula:    "000031",
-			precondition: true,
 		},
 		{
 			description:  "Tentativa de reverter um abono sem body",
-			header:       config.SetupHeadersAgente(),
-			setupBody:    false,
+			setupHeaders: config.SetupHeadersAgente(),
+			requestBody:  structs.PutAbonoBody{},
 			expected:     http.StatusBadRequest,
 			expectedDesc: "Chave \\\"Abonos\\\" não encontrada.",
-			nrInsc:       "10821992",
-			cpf:          "60515860409",
-			matricula:    "000031",
-			precondition: false,
 		},
 		{
 			description:  "Tentativa de reverter um abono sem header - Unauthorized",
-			header:       map[string]string{},
-			setupBody:    false,
+			setupHeaders: map[string]string{},
+			requestBody:  structs.PutAbonoBody{},
 			expected:     http.StatusUnauthorized,
 			expectedDesc: "Unauthorized",
 		},
@@ -54,36 +51,22 @@ func TestPutReveterAbono(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.description, func(t *testing.T) {
-			if tc.header != nil && tc.precondition {
-				precondition(tc.cpf, tc.nrInsc, tc.matricula)
+
+			if tc.setupHeaders != nil && len(tc.requestBody.Abonos) > 0 {
+				helper.CreateAbono(cnpj, taxID, matID, structs.PENDENTE)
 			}
+
 			api := config.SetupApi()
 
-			// Configura os parâmetros do corpo da requisição se necessário
-			var body interface{}
-			if tc.setupBody {
-				body = config.PutReveterAbonoBody(tc.cpf, tc.nrInsc, tc.matricula)
-			}
-
 			resp, err := api.Client.R().
-				SetHeaders(tc.header).
-				SetBody(body).
+				SetHeaders(tc.setupHeaders).
+				SetBody(tc.requestBody).
 				Put(api.EndpointsAgente["ReverterAbono"])
 
 			assert.NoError(t, err, "Erro ao fazer a requisição para %s", tc.description)
 			assert.Equal(t, tc.expected, resp.StatusCode(), "Status de resposta inesperado para %s", tc.description)
 			assert.Contains(t, string(resp.Body()), tc.expectedDesc, "Descrição de resposta inesperada")
-
 		})
 	}
-
-}
-
-func precondition(tax_id string, cnpj string, matricula string) {
-	api := config.SetupApi()
-	api.Client.R().
-		SetHeaders(config.SetupHeadersAgente()).
-		SetBody(config.PostSolicitaAbono2Body(tax_id, cnpj, matricula)).
-		Post(api.EndpointsAgente["Abono"])
 
 }
