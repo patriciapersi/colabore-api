b6938c876d3c24e68567ae5cc412a9c2e37930e6
Ton Arruda <georgem.juniore@gmail.com>
2024-06-25 09:25:27 -0300

    Inclusai estrutura de Férias

---
+++ config/agenteBody/BodyFerias.go
@@ -0,0 +1,37 @@
+package agentebody
+
+import (
+	"time"
+
+	"github.com/patriciapersi/colabore-api/config/structs"
+)
+
+func InformacoesFerias(cpf, nrInsc, matricula string) structs.ColaboradoresInfo {
+	return structs.ColaboradoresInfo{
+		CPF:                            cpf,
+		NrInscEmpregador:               nrInsc,
+		Matricula:                      matricula,
+		SolicitouAdiantamento13:        false,
+		DiasDisponiveisAbonoPecuniario: 10,
+		DiasDisponiveis:                30,
+		InicioPeriodoConcessivo:        "2022-06-29",
+		FimPeriodoConcessivo:           "2023-07-30",
+	}
+}
+
+func Ferias(cpf, nrInsc, matricula string) structs.Ferias {
+	return structs.Ferias{
+		Ferias: []structs.FeriasInfo{
+			{
+				CPF:                      cpf,
+				NrInscEmpregador:         nrInsc,
+				Matricula:                matricula,
+				SolicitouAdiantamento13:  true,
+				SolicitouAbonoPecuniario: false,
+				StatusSolicitacao:        1,
+				InicioPeriodoGozo:        time.Now().Format("2006-01-02"),
+				FimPeriodoGozo:           time.Now().AddDate(0, 0, 20).Format("2006-01-02"),
+			},
+		},
+	}
+}
